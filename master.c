f0bf53181df14f0e81db8958134d1f59075ae403
G Roques <groques360@gmail.com>
2017-09-17 10:08:48 -0500

    Add 60 second timer and Ctrl+C handler

--- master.c
+++ master.c
@@ -1,8 +1,45 @@
-#include <stdio.h>
+
+#include <errno.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <sys/time.h>
 
-int main (int argc, char *argv[]) {
+/* ARGSUSED */
+static void myhandler(int s) {
+  // TODO: Free memory
+  abort();
+}
 
-	return 0;
+static int setupinterrupt(void) {
+  /* set up myhandler for SIGPROF */
+  struct sigaction act;
+  act.sa_handler = myhandler;
+  act.sa_flags = 0;
+  return (sigemptyset( & act.sa_mask) || sigaction(SIGPROF, & act, NULL));
+}
+
+static int setupitimer(void) {
+  /* set ITIMER_PROF for 60-second interval */
+  struct itimerval value;
+  value.it_interval.tv_sec = 60;
+  value.it_interval.tv_usec = 0;
+  value.it_value = value.it_interval;
+  return (setitimer(ITIMER_PROF, & value, NULL));
+}
+
+int main(void) {
+  if (setupinterrupt() == -1) {
+    perror("Failed to set up handler for SIGPROF");
+    return 1;
+  }
+
+  if (setupitimer() == -1) {
+    perror("Faled to set up the ITIMER_PROF interval timer");
+    return 1;
+  }
+
+  signal(SIGINT, myhandler);
+  for ( ; ; ); /* execute rest of main program here */
 }
