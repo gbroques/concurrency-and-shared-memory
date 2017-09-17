17e1a79c691bf194a1527b0638e91b1500926656
G Roques <groques360@gmail.com>
2017-09-17 12:33:36 -0500

    Add command line arguments

--- palin.c
+++ palin.c
@@ -6,6 +6,16 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-int main(void) {
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s id xx\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+  int id, index;
+  id = atoi(argv[1]);
+  index = atoi(argv[2]);
 
+  printf("ID: %d Index: %d\n", id, index);
+
+  return EXIT_SUCCESS;
 }
