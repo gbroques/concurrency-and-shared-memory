45ef89fbfeebc710dc26f4c548b6b2ccfc5722ce
G Roques <groques360@gmail.com>
2017-09-17 12:34:54 -0500

    Add ability to read in strings and fork and exec child palin process

--- master.c
+++ master.c
@@ -5,10 +5,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+
+char* list[256];
+int numProcs = 1;
+
+void freeMemory(void);
+void readStrings(void);
+
 
 /* ARGSUSED */
 static void myhandler(int s) {
   // TODO: Free memory
+  freeMemory();
   abort();
 }
 
@@ -29,17 +42,63 @@ static int setupitimer(void) {
   return (setitimer(ITIMER_PROF, & value, NULL));
 }
 
+
 int main(void) {
   if (setupinterrupt() == -1) {
     perror("Failed to set up handler for SIGPROF");
-    return 1;
+    return EXIT_FAILURE;
   }
 
   if (setupitimer() == -1) {
     perror("Faled to set up the ITIMER_PROF interval timer");
-    return 1;
+    return EXIT_FAILURE;
   }
 
   signal(SIGINT, myhandler);
-  for ( ; ; ); /* execute rest of main program here */
+
+  // readStrings();
+
+  // freeMemory();
+
+  int pid, status;
+  pid = fork();
+
+  if (pid < 0) {
+    perror("fork()");
+    return EXIT_FAILURE;
+  }
+
+  if (pid == 0) { // Child
+    char procNum[12];
+    sprintf(procNum, "%d", numProcs);
+    execlp("palin", "palin", procNum, "0", (char*) NULL);
+    perror("palin");
+    _exit(EXIT_FAILURE);
+  }
+
+  wait(&status);
+  printf("palin terminated with status: %d\n", status);
+  return EXIT_SUCCESS;
+}
+
+void readStrings(void) {
+  int i = 0;
+  list[i] = (char*) malloc(256*sizeof(char));
+  while (scanf("%s", list[i]) > -1) {
+    int j, len;
+    for (j = 0, len = strlen(list[i]); j < len; j++) {
+      printf("%c", list[i][j]);
+    }
+    printf("\n");
+    i++;
+    list[i] = (char*) malloc(256*sizeof(char));
+  }
+}
+
+void freeMemory(void) {
+  int i = 0;
+  while (list[i] != NULL) {
+    free(list[i]);
+    i++;
+  }
 }
